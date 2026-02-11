7481f530e35fb3f6b023ff04b79b0b8efc51e6c5
genxcode <94076315+gen-x13@users.noreply.github.com>
2026-02-11 15:09:04 +0400

    Update main.py

--- main.py
+++ main.py
@@ -47,6 +47,35 @@ selected=option_menu(
         orientation="horizontal"
 )   
 
+video_html = """
+		<style>
+
+		#myVideo {
+		  position: fixed;
+		  right: 0;
+		  bottom: 0;
+		  min-width: 100%; 
+		  min-height: 100%;
+		}
+
+		.content {
+		  position: fixed;
+		  bottom: 0;
+		  background: rgba(0, 0, 0, 0.5);
+		  color: #f1f1f1;
+		  width: 100%;
+		  padding: 20px;
+		}
+
+		</style>	
+		<video autoplay muted loop id="myVideo">
+		  <source src="https://media.tenor.com/KzdfR6Hek7YAAAAM/forest-rain.gif">
+		  Your browser does not support HTML5 video.
+		</video>
+        """
+
+st.markdown(video_html, unsafe_allow_html=True)
+
 # -------------------------------   Bird's data   --------------------------- #
 
 # Security preventing any reading problem and any cache data problem
