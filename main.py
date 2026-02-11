c904b602c743f48b93f2adbab2eeaa7e8af50540
genxcode <94076315+gen-x13@users.noreply.github.com>
2026-02-11 15:18:41 +0400

    Update main.py

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ base_path = Path(__file__).parent
         
 # Page Icon, side bar collpase
 st.set_page_config(page_title="Clustering Sound And More...", 
-                   initial_sidebar_state="collapsed",
-                   layout="wide")
+                   initial_sidebar_state="collapsed")
+                   #layout="wide")
 
 # Horizontal menu
 selected=option_menu(
