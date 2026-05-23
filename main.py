051307b00c8b34b9174d3d174c969ddd628f2a80
genxcode <94076315+gen-x13@users.noreply.github.com>
2026-05-23 18:32:33 +0400

    Update main.py

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ if selected == "Wildlife":
     st.markdown("<b><p style='color:black;font-size:20px;background:rgba(255,255,255,0.20);border-radius:12px;padding:10px 14px;display:inline;backdrop-filter:blur(6px);'>This app groups birds based on their birdsong. Select how many clusters you want to observe within the ~400 audios from Xeno Canto.</p></b>",
                 unsafe_allow_html=True)
 
-    col1, col2 = st.columns([3, 1])
+    col1, col2 = st.columns([4, 1])
 
     # Buttons from clustering to dashboard
     with col2:
