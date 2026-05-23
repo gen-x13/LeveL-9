2385e560dd5e592d38866bfe7d5dc87251c775d6
genxcode <94076315+gen-x13@users.noreply.github.com>
2026-05-23 18:20:31 +0400

    Update main.py

--- main.py
+++ main.py
@@ -292,7 +292,8 @@ if selected == "Wildlife":
                                 #st.write(selected_row["Spectral_Centroid"])
                                 #st.write(selected_row)
                                 st.write(sample_similarity)
-                                st.caption(top5)
+                                st.subheader("5 approximatively closest sounds with {spe_bird_sel}")
+                                st.markdown(top5)
                                 # for idx, sim in enumerate(spectr_similarity):
                                 
                         with subcol2:
